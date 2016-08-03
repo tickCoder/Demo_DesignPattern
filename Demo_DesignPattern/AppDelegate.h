7c2378473f32ef1093b2a4f06ea13492d349e22d
tickCoder <tickCoder@users.noreply.github.com>
2016-08-03 22:38:16 +0800

    Initial Commit

---
+++ Demo_DesignPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_DesignPattern
+//
+//  Created by Claris on 2016.08.03.Wednesday.
+//  Copyright © 2016 tickCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo_DesignPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo_DesignPattern
+//
+//  Created by Claris on 2016.08.03.Wednesday.
+//  Copyright © 2016 tickCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
